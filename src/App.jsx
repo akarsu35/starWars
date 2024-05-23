52a60317a18a3a17c9aca97ce4526a0ef2a4876f
akarsu35 <cumhurakarsu@gmail.com>
2024-05-23 18:40:41 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from 'react'
+
+import './App.css'
+import axios from 'axios'
+
+import StarshipList from './components/StarshipList'
+
+function App() {
+  
+  return (
+    <>
+    
+      <StarshipList/>
+    </>
+  )
+}
+
+export default App
